997330947ea3a6e6e99fd08d046f85daf3426f1f
Bumpeet <pavanchetan81@gmail.com>
2023-08-05 17:36:03 +0530

    Commit 1

---
+++ app.py
@@ -0,0 +1,46 @@
+import torch
+from torchvision.transforms import transforms
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam import GradCAM
+import numpy as np
+from matplotlib import pyplot as plt
+import streamlit as st
+from myModel import MNISTmodel
+from PIL import Image, ImageOps
+
+@st.cache_resource  # 👈 Add the caching decorator
+def load_model():
+    model = torch.load(r"MNIST.pt")
+    model.eval()
+    target_layers = [model.conv1]
+    cam = GradCAM(model=model, target_layers=target_layers)
+
+    return model, cam
+
+def main():
+    # loading the cam model and MNIST_model 
+    model, cam = load_model()
+    
+    st.header("Space to Visualize the GradCam for a Number")
+    st.divider()
+    uploaded_file = st.file_uploader("Upload the image to classify it and also to visulaize it's Grad Cam")
+
+    if uploaded_file is not None:
+        # Reading the uploaded image into pil and then into torch.tensor format
+        pil_img = ImageOps.grayscale(Image.open(uploaded_file).resize((28,28)))
+        torch_tensor = transforms.ToTensor()(pil_img).view(-1,1,*pil_img.size)
+
+        # generating the heat map using cam model
+        gray_scale_cam = cam(input_tensor=torch_tensor)
+        prediction = model(torch_tensor)
+
+
+        in_img_vis = torch_tensor.permute(0,2,3,1).numpy() # converting this into B, W, H, C 
+        #this line combines the original image and the heat map generated using the cam model
+        visualization = show_cam_on_image(in_img_vis, np.transpose(gray_scale_cam,(1,2,0)),image_weight=0.7)  
+
+        st.image(Image.fromarray(visualization.squeeze()).resize((480,480)))
+        st.info(f"The model has predicted this to be a {torch.argmax(prediction).item()}")
+        
+if __name__=="__main__":
+    main()
